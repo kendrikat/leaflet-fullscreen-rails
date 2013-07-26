b2142394bda5a6be7a82fb826c155e82ad0bba4f
Klaas Endrikat <klaas.endrikat@googlemail.com>
2013-07-26 20:08:32 +0200

    init

---
+++ lib/leaflet-fullscreen-rails.rb
@@ -0,0 +1,9 @@
+require "leaflet-fullscreen-rails/version"
+
+module Leaflet
+  module Fullscreen
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/leaflet-fullscreen-rails/version.rb
@@ -0,0 +1,7 @@
+module Leaflet
+  module Fullscreen
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
